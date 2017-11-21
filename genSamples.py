fafcf3598144f28ba465a2e12e8e78c48ff4488d
yy <617302261@qq.com>
2017-11-21 16:24:06 +0800

    generate samples from feature files

---
+++ genSamples.py
@@ -0,0 +1,64 @@
+import os
+import csv
+
+def processWord(word):
+	newWord = ''
+	if 'Statement' in word:
+			newWord = word.split(' Statement')[0]
+	elif 'Clause' in word:
+		newWord = word.split(' Clause')[0]
+	if ':' in word:
+		newWord = word.split(': ')[1].split('<')[0]
+	return newWord
+
+def writeTrain(rows, csvPath):
+	with open(csvPath, 'w') as csvfile:
+		writer = csv.writer(csvfile)
+		for row in rows:
+			if 'bug_' in row[-1]:
+				row.append(1)
+			else:
+				row.append(0)
+			writer.writerow(row)
+	print 'done.'
+
+def writeTest(rows, csvPath):
+	with open(csvPath, 'w') as csvfile:
+		writer = csv.writer(csvfile)
+		for row in rows:
+			writer.writerow(row)
+	print 'done.'
+
+def genSamples(featurePath, dictPath, csvPath, opt):
+	f = open(dictPath)
+	words = f.readlines()
+	f.close()
+	samples = []
+	filenames = os.listdir(featurePath)
+	for filename in filenames:
+		sample = []
+		f = open(featurePath+filename, 'r')
+		lines = f.readlines()
+		f.close()
+		for i in range(len(lines)):
+			word = processWord(lines[i])
+			if word and word in words:
+				sample.append(words.index(word)+1)
+			else:
+				sample.append(len(words)+1)
+				words.append(word)
+		if len(sample) > 200:
+			sample = sample[:200]
+		elif len(sample) < 200:
+			for i in range(len(sample), 200):
+				sample.append(0)
+		sample.append(filename)
+		samples.append(sample)
+	if opt == 'train':
+		writeTrain(samples, csvPath)
+	elif opt == 'test':
+		writeTest(samples, csvPath)
+
+
+if __name__ == '__main__':
+	genSamples('/media/yy/10A4078410A40784/grad_proj/exp/camel_1.4.0/', '/media/yy/10A4078410A40784/grad_proj/exp/dicts/dict_camel_1.4.0', '/media/yy/10A4078410A40784/grad_proj/exp/dicts/camel_train_1.4.0.csv', 'train')
