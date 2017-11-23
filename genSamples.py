31dab546c4c60bfb34577f8cec598e22c4269fad
yy <617302261@qq.com>
2017-11-23 17:12:47 +0800

    remove writeTest(), unified with writeTrain()

--- genSamples.py
+++ genSamples.py
@@ -17,19 +17,14 @@ def writeTrain(rows, csvPath):
 		for row in rows:
 			if 'bug_' in row[-1]:
 				row.append(1)
+				row.append(0)
 			else:
 				row.append(0)
+				row.append(1)
 			writer.writerow(row)
 	print 'done.'
 
-def writeTest(rows, csvPath):
-	with open(csvPath, 'w') as csvfile:
-		writer = csv.writer(csvfile)
-		for row in rows:
-			writer.writerow(row)
-	print 'done.'
-
-def genSamples(featurePath, dictPath, csvPath, opt):
+def genSamples(featurePath, dictPath, csvPath):
 	f = open(dictPath)
 	words = f.readlines()
 	f.close()
@@ -54,11 +49,8 @@ def genSamples(featurePath, dictPath, csvPath, opt):
 				sample.append(0)
 		sample.append(filename)
 		samples.append(sample)
-	if opt == 'train':
-		writeTrain(samples, csvPath)
-	elif opt == 'test':
-		writeTest(samples, csvPath)
+	writeTrain(samples, csvPath)
 
 
 if __name__ == '__main__':
-	genSamples('/media/yy/10A4078410A40784/grad_proj/exp/camel_1.4.0/', '/media/yy/10A4078410A40784/grad_proj/exp/dicts/dict_camel_1.4.0', '/media/yy/10A4078410A40784/grad_proj/exp/dicts/camel_train_1.4.0.csv', 'train')
+	genSamples('/media/yy/10A4078410A40784/grad_proj/exp/camel_1.6.0/', '/media/yy/10A4078410A40784/grad_proj/exp/dicts/dict_camel_1.2.0', '/media/yy/10A4078410A40784/grad_proj/exp/dicts/camel_test_1.6.0.csv')
