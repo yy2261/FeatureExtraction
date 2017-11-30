cfed25d4e0798f39aedd7bd97b0fe97be6681ba0
yy <617302261@qq.com>
2017-11-30 15:52:49 +0800

    consider one name in csv refer to >1 in featuredir

--- fileLabel.py
+++ fileLabel.py
@@ -1,23 +1,40 @@
-import pandas
-import os
+'''
+yy grad proj source code
+label files with bug info
+'''
 
-def checkFileExists(featurePath, csvPath):
+
+import os
+import pandas
+import sys
+
+
+def selectFile(csvPath, featurePath):
 	f = open(csvPath, 'r')
 	csv = pandas.read_csv(f)
 	csvdirs = csv['dirname']
 	defects = csv['bug']
 	f.close()
-	filenames = os.listdir(featurePath)
+	defectNum = 0
+	featuredirs = os.listdir(featurePath)
 	for i in range(len(csvdirs)):
-		exists = 0
-		for filename in filenames:
-			if csvdirs[i] in filename:
-				if defects[i] > 0:
-					os.system('mv '+featurePath+filename+' '+featurePath+'bug_'+filename)
-				exists = 1
-				break
-		if exists == 0:
-			print csvdirs[i]
+		fileName = csvdirs[i].split('.')[-1]
+		num = 0
+		fileList = []
+		for featuredir in featuredirs:
+			featurefileName = featuredir.split('.')[-2]
+			if fileName == featurefileName:
+				num += 1
+				fileList.append(featuredir)
+		if num == 1:
+			if defects[i] == 1:
+				os.system('mv '+featurePath+fileList[0]+' '+featurePath+'bug_'+fileList[0])
+		if num > 1:
+			if defects[i] == 1:
+				print num
+				print csvdirs[i]
+				print fileList
+
 
 if __name__ == '__main__':
-	checkFileExists('/media/yy/10A4078410A40784/grad_proj/exp/camel_1.6.0/', '/media/yy/10A4078410A40784/grad_proj/exp/defectInfo/camel-1.6.csv')
+	selectFile(sys.argv[1], sys.argv[2])
