e92fab93e0d586a34a34200e6184206be0e48e19
yy <617302261@qq.com>
2017-12-20 14:29:59 +0800

    rename genWocab.py, print some message

---
+++ genVocab.py
@@ -0,0 +1,46 @@
+import os
+import sys
+from tokenStem import *
+
+
+def writeText(fd, lines):
+	for i in range(len(lines)):
+		if ' Declaration' in lines[i]:
+			continue
+		elif 'Statement' in lines[i] or 'Clause' in lines[i]:
+			fd.write(' '+lines[i].split(' ')[0].lower())
+		else:
+			name = lines[i].split(':')[-1]
+			wordList = splittt(name)
+			wordList = stemming(wordList)
+			phrase = conj(wordList)
+			fd.write(' '+phrase)
+	fd.write('\n')
+
+
+def getFile(path):
+	featureFiles = []
+	dirs = os.listdir(path)
+	for dir_ in dirs:
+		fileList = os.listdir(path+dir_)
+		for item in fileList:
+			featureFiles.append(path+dir_+'/'+item)
+	print len(featureFiles)
+	return featureFiles
+
+def getText(path):
+	featureFiles = getFile(path)
+	g = open('text.txt', 'w')
+	num = 0
+	for featureFile in featureFiles:
+		fd = open(featureFile, 'r')
+		lines = fd.read().split('\n')
+		fd.close()
+		writeText(g, lines)
+		print str(num)+'\t'
+		num += 1 
+	g.close()
+
+
+if __name__ == '__main__':
+	getText(sys.argv[1])
