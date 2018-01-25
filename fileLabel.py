0518460d643d0be422cc28333a2f472e8f785ea8
yy <617302261@qq.com>
2018-01-25 09:17:40 +0800

    fix when two files share one name

--- fileLabel.py
+++ fileLabel.py
@@ -19,21 +19,28 @@ def selectFile(csvPath, featurePath):
 	featuredirs = os.listdir(featurePath)
 	for i in range(len(csvdirs)):
 		fileName = csvdirs[i].split('.')[-1]
+		dirName = csvdirs[i].split('.')[-2]
+		secondDirName = csvdirs[i].split('.')[-3]
 		num = 0
 		fileList = []
 		for featuredir in featuredirs:
 			featurefileName = featuredir.split('.')[-2]
-			if fileName == featurefileName:
+			featureDirName = featuredir.split('.')[-3]
+			featureSecondDirName = featuredir.split('.')[-4]
+			if fileName == featurefileName and dirName == featureDirName and secondDirName == featureSecondDirName:
 				num += 1
 				fileList.append(featuredir)
 		if num == 1:
 			if defects[i] == 1:
 				os.system('mv '+featurePath+fileList[0]+' '+featurePath+'bug_'+fileList[0])
-		if num > 1:
+		elif num > 1:
 			if defects[i] == 1:
 				print num
 				print csvdirs[i]
 				print fileList
+		elif num == 0 and defects[i] == 1:
+			print csvdirs[i]
+	print 'done.'
 
 
 if __name__ == '__main__':
