ac99f426c4f2fcf2072164f2ccf69b987b582625
yy2261 <617302261@qq.com>
2018-01-30 15:42:54 +0800

    add logistic regression results

--- scripts/runDbnTest.sh
+++ scripts/runDbnTest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # remove the logs generated last time
-rm /home/yy/.yadlt/models
+rm -r /home/yy/.yadlt/models
 rm -r /home/yy/.yadlt/logs
 
 echo "input project name:"
@@ -15,6 +15,7 @@ for i in 1 2 3 4 5
 do
 	echo "this is $i/5 times..."
 	python /media/yy/10A4078410A40784/grad_proj/code/run_dbn.py /media/yy/10A4078410A40784/grad_proj/exp/dicts/${ProjName}_${Version_1}.Sample.npy /media/yy/10A4078410A40784/grad_proj/exp/dicts/${ProjName}_${Version_1}.Label.npy /media/yy/10A4078410A40784/grad_proj/exp/dicts/${ProjName}_${Version_2}.Sample.npy /media/yy/10A4078410A40784/grad_proj/exp/dicts/${ProjName}_${Version_2}.Label.npy /media/yy/10A4078410A40784/grad_proj/exp/dicts/${ProjName}_${Version_1}.DBN.npy /media/yy/10A4078410A40784/grad_proj/exp/dicts/${ProjName}_${Version_2}.DBN.npy
+	python /media/yy/10A4078410A40784/grad_proj/code/logistic.py /media/yy/10A4078410A40784/grad_proj/exp/dicts/${ProjName}_${Version_1}.DBN.npy /media/yy/10A4078410A40784/grad_proj/exp/dicts/${ProjName}_${Version_1}.Label.npy /media/yy/10A4078410A40784/grad_proj/exp/dicts/${ProjName}_${Version_2}.DBN.npy /media/yy/10A4078410A40784/grad_proj/exp/dicts/${ProjName}_${Version_2}.Label.npy
 done
 
 echo "*********refer to tensorboard**********"
