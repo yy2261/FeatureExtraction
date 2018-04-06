f42cea795908a35b2cc9b033b545e0f4b00abd6d
yy2261 <617302261@qq.com>
2018-04-06 15:00:53 +0800

    write several features into one file

---
+++ utils/copyFile.py
@@ -0,0 +1,23 @@
+
+import sys
+import os
+
+
+'''
+write files in a directory into one file
+'''
+
+def main(path, writePath):
+	names = os.listdir(path)
+	with open(writePath, 'wb') as writeFd:
+		for name in names:
+			name = path+name
+			with open(name, 'r') as readFd:
+				for line in readFd:
+					if line != ' \n':
+						writeFd.write(line)
+
+
+
+if __name__ == '__main__':
+	main(sys.argv[1], sys.argv[2])
