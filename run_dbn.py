d9528a81d3c0016397a5dc16a1faf9eed651e54f
yy2261 <617302261@qq.com>
2018-06-15 09:00:39 +0800

    little change for tuning the parameters

--- run_dbn.py
+++ run_dbn.py
@@ -7,11 +7,19 @@ from yadlt.utils import datasets, utilities
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import *
 
+rbm_1 = float(sys.argv[5])
+rbm_2 = float(sys.argv[6])
+rbm_3 = float(sys.argv[7])
+rbm_4 = float(sys.argv[8])
+rbm_stddev = float(sys.argv[9])
+logistic_lr = float(sys.argv[10])
 
-rbm_layers = [100, 100, 100, 100, 100, 100, 100, 100, 100, 100]
-rbm_learning_rate = [0.001, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
+rbm_layers = [100, 100, 100, 100]
+rbm_learning_rate = [rbm_1, rbm_2, rbm_3, rbm_4]
+#rbm_layers = [100, 100, 100, 100, 100, 100, 100, 100, 100, 100]
+#rbm_learning_rate = [rbm_1, rbm_2, rbm_3, rbm_4, 0.07, 0.07, 0.05, 0.05, 0.05, 0.05]
 rbm_num_epochs = [200]
-rbm_batch_size = [50]
+rbm_batch_size = [10]
 rbm_gibbs_k = [1]
 finetune_opt = 'adam'     # sgd/adagrad/momentum/adam
 finetune_loss_func = 'softmax_cross_entropy'        # softmax_cross_entropy/mse 
@@ -36,16 +44,14 @@ if __name__ == '__main__':
         rbm_num_epochs=rbm_num_epochs,
         rbm_gibbs_k = rbm_gibbs_k,
         rbm_gauss_visible=True,
-        rbm_stddev=50,
+        rbm_stddev=rbm_stddev,
         momentum=0.9,
         rbm_batch_size=rbm_batch_size,
 	)
 
     train_result, valid_result = srbm.pretrain(trX, trY, vlX, vlY)
 
-    print train_result, valid_result
-
-    clf=LogisticRegression(penalty='l1', C=100, solver='liblinear')
+    clf=LogisticRegression(penalty='l1', C=logistic_lr, solver='liblinear')
 
     trY = [item[0] for item in trY]
     vlY = [item[0] for item in vlY]
