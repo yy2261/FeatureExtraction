31cc95551f5dd26fff43fbc93795c05823ed889c
yy2261 <617302261@qq.com>
2018-06-18 11:04:53 +0800

    add mean pooling, fix test data step length

--- LSTM.py
+++ LSTM.py
@@ -8,39 +8,56 @@ import random
 
 # hyperparameters
 global lr, training_iters, batch_size, n_inputs, n_steps, n_hidden_units, n_classes
-lr = 0.000001
-training_iters = 2000
+lr = 0.0005
+training_iters = 100
 batch_size = 20
       
 n_inputs = 200
-n_steps = 40
+n_steps = 0
 n_hidden_units = 30  # neurons in hidden layer  
 n_classes = 2      # classes
       
 
 def processData(train_set, test_set):
-    global lr, training_iters, batch_size, n_inputs, n_steps, n_hidden_units, n_classes
+    global lr, batch_size, n_inputs, n_steps, n_hidden_units
 
     train_X = train_set[:, :-2]
     train_Y = train_set[:, -2:]
     test_X = test_set[:, :-2]
     test_Y = test_set[:, -2:]
 
+    train_mask = np.zeros((n_steps, len(train_X), n_hidden_units))
     # transform train_X into np.array
     shape_X = np.zeros((len(train_X), n_steps, n_inputs))
     for i in range(len(train_X)):
         for j in range(n_steps):
+            
             shape_X[i, j] = np.array(train_X[i][j][:])
+            
+            if train_X[i][j] != [0.0]*n_inputs:
+                train_mask[j][i] = np.ones(n_hidden_units)
+    
     train_X = shape_X[:]
+
+    # transform test_X into np.array, if length of test_X is smaller than train_X then padding
+    test_mask = np.zeros((n_steps, len(test_X), n_hidden_units))
     shape_X = np.zeros((len(test_X), n_steps, n_inputs))
+
     for i in range(len(test_X)):
         for j in range(n_steps):
-            shape_X[i, j] = np.array(test_X[i][j][:])
+            if len(test_X[i]) > j:
+                shape_X[i, j] = np.array(test_X[i][j][:])
+            else:
+                shape_X[i, j] = [0.0]* n_inputs
+
+            if len(test_X[i]) > j and test_X[i][j] != [0.0] * n_inputs:
+                test_mask[j][i] = np.ones(n_hidden_units)
+
     test_X = shape_X[:]
-    return train_X, train_Y, test_X, test_Y
+    return train_X, train_Y, train_mask, test_X, test_Y, test_mask
       
 def RNN(x, weight, weight_out, bias, bias_out, mask): 
-    global lr, training_iters, batch_size, n_inputs, n_steps, n_hidden_units, n_classes
+    global batch_size, n_inputs, n_steps, n_hidden_units
 
     # calculate input of lstm
     x = tf.reshape(x, [-1, n_inputs])
@@ -68,7 +85,7 @@ def RNN(x, weight, weight_out, bias, bias_out, mask):
 
 
 def buildModel():
-    global lr, training_iters, batch_size, n_inputs, n_steps, n_hidden_units, n_classes
+    global lr, n_inputs, n_steps, n_hidden_units, n_classes
     # tf Graph input
     x = tf.placeholder(tf.float32, [None, n_steps, n_inputs])
     y = tf.placeholder(tf.float32, [None, n_classes])
@@ -92,31 +109,13 @@ def buildModel():
     return train_op, cost, x, y, mask, accuracy, pred
 
 
-def selectData(train_X, train_Y, test_X, test_Y):
-    # input: training set and test set
-    # output: xs, ys, xs_test, ys_test in np.array form
-    global lr, training_iters, batch_size, n_inputs, n_steps, n_hidden_units, n_classes
-    xs, ys, xs_test, ys_test = [], [], [], []
-    while len(xs) < batch_size/2:
-        rand = np.random.randint(len(train_X))
-        if train_Y[rand][0] == 0:
-            xs.append(train_X[rand])
-            ys.append(train_Y[rand])
-    while len(xs) < batch_size:
-        rand = np.random.randint(len(train_X))
-        if train_Y[rand][0] == 1:
-            xs.append(train_X[rand])
-            ys.append(train_Y[rand])
-    for num in range(batch_size):
-        rand = np.random.randint(len(test_X))
-        xs_test.append(test_X[rand])
-        ys_test.append(test_Y[rand])
-    # reshape xs and xs_test for feed_dict
-    xs = np.array(xs).reshape([-1, n_steps, n_inputs])
-    ys = np.array(ys)
-    xs_test = np.array(xs_test).reshape([-1, n_steps, n_inputs])
-    ys_test = np.array(ys_test)
-    return xs, ys, xs_test, ys_test
+def genTrainBatch(train_set):
+    # input: training set (zip of train_X, train_Y and train_mask)
+    global batch_size
+    batches = len(train_set) // batch_size
+    for i in range(batches):
+        yield train_set[i:i+batch_size]
+
 
 def makePlot(train_result, test_result):
     plt.figure()
@@ -124,56 +123,54 @@ def makePlot(train_result, test_result):
     plt.plot(test_result)
     plt.show()
 
-def train(train_op, cost, x, y, mask, accuracy, train_X, train_Y, test_X, test_Y):
-    global lr, training_iters, batch_size, n_inputs, n_steps, n_hidden_units, n_classes
+def train(train_op, cost, x, y, mask, accuracy, train_X, train_Y, train_mask):
+    global training_iters
     # init the model
     init = tf.global_variables_initializer()
     saver = tf.train.Saver()
     train_result = []
-    test_result = []
 
     with tf.Session() as sess:
         sess.run(init)
-        step = 0
         for i in range(training_iters):
-            xs , ys, xs_test, ys_test = selectData(train_X, train_Y, test_X, test_Y)
-            masks = np.array([[[1]*n_hidden_units]*batch_size]*n_steps)
-    # calculate cost
-            _, loss_ = sess.run([train_op, cost], feed_dict={x: xs, y: ys, mask: masks})
-            if step % 10 == 0:
-                train_result.append(sess.run(accuracy, feed_dict={x: xs, y: ys, mask: masks}))
-                test_result.append(sess.run(accuracy, feed_dict={x: xs_test, y: ys_test, mask: masks}))
+            train_set = zip(train_X, train_Y, np.swapaxes(train_mask, 0, 1))
+            np.random.shuffle(train_set)
+            batches = [ _ for _ in genTrainBatch(train_set)]
+
+            for batch in batches:
+                batch = zip(*batch)
+                xs, ys, masks = batch[0], batch[1], batch[2]
+                # calculate cost
+                _, loss_ = sess.run([train_op, cost], feed_dict={x: xs, y: ys, mask: np.swapaxes(masks, 0, 1)})
+            if i % 10 == 0:
                 print 'loss is: '+str(loss_)
-            step += 1
+
         saver.save(sess, 'models/model.ckpt')
-    makePlot(train_result, test_result)
     return saver
 
-def predict(saver, pred, x, mask, test_X, test_Y):
-    global lr, training_iters, batch_size, n_inputs, n_steps, n_hidden_units, n_classes
+def predict(saver, pred, x, mask, test_X, test_Y, test_mask):
+    global batch_size, n_inputs, n_steps
+
     result = []
     with tf.Session() as sess:
         saver.restore(sess, 'models/model.ckpt')
         start = 0
         end = start + batch_size
-        masks = np.array([[[1]*n_hidden_units]*batch_size]*n_steps)
         while end < len(test_X):
             result_batch = sess.run(pred, feed_dict={
-                x: test_X[start:end].reshape([-1, n_steps, n_inputs]), mask: masks})
+                x: test_X[start:end].reshape([-1, n_steps, n_inputs]), mask: test_mask[:, start:end, :]})
             start += batch_size
             end = start + batch_size
             for i in range(batch_size):
                 result.append(result_batch[i])
         # last result
         result_last_batch = sess.run(pred, feed_dict={
-            x: test_X[len(test_X)-batch_size:].reshape([-1, n_steps, n_inputs]), mask: masks})
+            x: test_X[len(test_X)-batch_size:].reshape([-1, n_steps, n_inputs]), mask: test_mask[:, len(test_X)-batch_size:, :]})
         for i in range(len(test_X)-start):
             result.append(result_last_batch[i])
     return result
 
 def measure(result, test_Y):
-    global lr, training_iters, batch_size, n_inputs, n_steps, n_hidden_units, n_classes
-    
     tp, fp, fn, tn = 0, 0, 0, 0
     for i in range(len(result)):
         predict = result[i]
@@ -192,15 +189,21 @@ def measure(result, test_Y):
     print 'recall is:'+str(recall)
     print 'f1 is:'+str(f1)
 
+
 def main(train_path, test_path):
+    global n_steps
     train_set = np.load(train_path)
     test_set = np.load(test_path)
-    train_X, train_Y, test_X, test_Y = processData(train_set, test_set)
-    n_steps = len(train_X[0])
+    n_steps = len(train_set[0])-2
+    print 'processing data...'
+    train_X, train_Y, train_mask, test_X, test_Y, test_mask = processData(train_set, test_set)
+    print 'building model...'
     train_op, cost, x, y, mask, accuracy, pred = buildModel()
-    print 'going to train...'
-    saver = train(train_op, cost, x, y, mask, accuracy, train_X, train_Y, test_X, test_Y)
-    result = predict(saver, pred, x, mask, test_X, test_Y)
+    print 'training...'
+    saver = train(train_op, cost, x, y, mask, accuracy, train_X, train_Y, train_mask)
+    print 'predicting...'
+    result = predict(saver, pred, x, mask, test_X, test_Y, test_mask)
+    print 'measure results...'
     measure(result, test_Y)
 
 if __name__ == '__main__':
